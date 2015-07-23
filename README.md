06feaa654c591ac19827fdfbdedfacd8a0eea042
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2015-07-23 12:57:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# hklight
+
+This is a sample project of a HomeKit light bulb using [HomeControl](https://github.com/brutella/hc). [Read more](#TODO).
+
+# Installation
+
+## Go
+
+Make sure that you have a working [Go installation](http://golang.org/doc/install).
+
+## Checkout
+
+- Clone project `git clone https://github.com/brutella/hklight && cd hklight`
+- Install dependencies `go get`
+
+## Build
+
+Build with `go build hklightd.go`.
+
+## Run
+
+Execute the executable `./hklightd` or run with `go run hklightd.go`.
+
---
+++ hklightd.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/brutella/hc/hap"
+	"github.com/brutella/hc/model"
+	"github.com/brutella/hc/model/accessory"
+	"log"
+)
+
+func turnLightOn() {
+	log.Println("Turn Light On")
+}
+
+func turnLightOff() {
+	log.Println("Turn Light Off")
+}
+
+func main() {
+	info := model.Info{
+		Name:         "Personal Light Bulb",
+		Manufacturer: "Matthias",
+	}
+
+	light := accessory.NewLightBulb(info)
+	light.OnStateChanged(func(on bool) {
+		if on == true {
+			turnLightOn()
+		} else {
+			turnLightOff()
+		}
+	})
+
+	t, err := hap.NewIPTransport("32191123", light.Accessory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t.Start()
+}
